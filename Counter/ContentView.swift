d5f3f2421fe4c8ec839ccb61bbab014fdbe36bae
Chelzo <ccalvo0314@gmail.com>
2024-12-12 14:10:34 -0500

    Initial Commit: Counter App Setup

---
+++ Counter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Counter
+//
+//  Created by Chelsea Calvo on 12/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Counter/CounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterApp.swift
+//  Counter
+//
+//  Created by Chelsea Calvo on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
